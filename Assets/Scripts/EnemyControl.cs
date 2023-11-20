d4e39ae8f732242311d97d65d78b3e99849475eb
Puddin26 <jl12476@nyu.edu>
2023-11-19 22:15:53 -0500

    Added Enemy Prototype
    An enemy on floor, detects platform edges and turn around. Currently not interactable.

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+
+    [SerializeField] private float moveSpeed;
+    public Transform groundDetection;
+    private bool movingRight = true;
+
+    // Layer mask to detect platforms
+    private LayerMask platformLayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        platformLayerMask = LayerMask.GetMask("Platform");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        else{
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
         
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f, platformLayerMask);
+        Debug.DrawLine(groundDetection.position, groundDetection.position + new Vector3(0, -1f, 0), Color.red);
+
+        if (groundInfo.collider == true)
+        {
+            Debug.Log("On Platform");
+        }
+        if (groundInfo.collider == false)
+        {
+            Debug.Log("On Edge");
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = true;
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -63,7 +63,7 @@ public class PlayerController : MonoBehaviour
             dash();
         }
         #endregion
-
+          
         
 
     }
