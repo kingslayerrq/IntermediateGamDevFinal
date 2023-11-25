6de5a24a6edcb8f1596698bc88072e0b93287f45
kingslayerrq <ruizhequ813@gmail.com>
2023-11-25 18:42:06 -0500

    implemented soul gauge ui

--- Assets/Scripts/PlayerScripts/PlayerUI.cs
+++ Assets/Scripts/PlayerScripts/PlayerUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,13 @@ public class PlayerUI : MonoBehaviour
 {
     private Player player;
 
-    [Header("UI Elements")]
+    [Header("Health UI")]
     public Transform heartLayoutGroup;
     public GameObject heartPrefab;
+
+    [Header("Soul Gauge UI")]
+    public Image SoulGaugeFill;
+    [SerializeField] private float curFillAmount;
 
     private List<GameObject> heartList;
     [SerializeField] private int curHeartNumDisplayed;
@@ -22,24 +27,30 @@ public class PlayerUI : MonoBehaviour
         // Subscribe to player instance events
         player.onHit += removeHeart;
         player.onRecover += addHeart;
+        player.onEnergyRecover += increaseGauge;
+        player.onEnergySpent += decreaseGauge;
     }
 
     private void Start()
     {
         heartList = new List<GameObject>();
-        // initialize the hearts
+        // Initialize Hearts
         Debug.Log(player.maxHealth);
         for(int i = 0; i < player.maxHealth; i++)
         {
             heartList.Add(Instantiate(heartPrefab, heartLayoutGroup));
         }
+        // Initialize Soul Gauge
+        SoulGaugeFill.fillAmount = player.curSlowGauge;
     }
 
     private void Update()
     {
         curHeartNumDisplayed = heartLayoutGroup.childCount;
+        curFillAmount = SoulGaugeFill.fillAmount;
     }
 
+    #region Health UI
     // Add X heart to List
     void addHeart(int num)
     {
@@ -64,10 +75,26 @@ public class PlayerUI : MonoBehaviour
             }
         }
     }
-    private void OnDestroy()
+    #endregion
+    #region Soul Gauge UI
+    void increaseGauge(float amount)            // Parameter {0 - 1}
+    {
+        float endAmount = Mathf.Clamp(SoulGaugeFill.fillAmount + amount, 0.0f, 1.0f);
+        SoulGaugeFill.DOFillAmount(endAmount, Time.deltaTime);                // we call this function each frame when not slowing down time and not at full gauge
+    }
+    void decreaseGauge(float amount)            // Parameter {0 - 1}
+    {
+        float endAmount = Mathf.Clamp(SoulGaugeFill.fillAmount - amount, 0.0f, 1.0f);
+        SoulGaugeFill.DOFillAmount(endAmount, Time.unscaledDeltaTime);
+    }
+    #endregion
+
+    private void OnDestroy()                    
     {
         // Unsubscribe to player instance events
         player.onHit -= removeHeart;
         player.onRecover -= addHeart;
+        player.onEnergyRecover -= increaseGauge;
+        player.onEnergySpent -= decreaseGauge;
     }
 }
--- Assets/Scripts/PlayerScripts/TimeSlow.cs
+++ Assets/Scripts/PlayerScripts/TimeSlow.cs
@@ -4,7 +4,9 @@ public class TimeSlow : MonoBehaviour
 {
     public float slowDownFactor { get; set; }
     public float slowDownDuration { get; set; }
-   
+    [SerializeField] private float slowDownRecoverOffset;
+
+    //private float recoverTime = 0;
 
     public float minSlowDownDuration = 2f;
     private void Update()
@@ -12,15 +14,23 @@ public class TimeSlow : MonoBehaviour
         if (slowDownDuration != 0)
         {
             
-            Time.timeScale += (1f / slowDownDuration) * Time.unscaledDeltaTime;
+            Time.timeScale += (1f / (slowDownDuration + slowDownRecoverOffset)) * Time.unscaledDeltaTime;
             Time.timeScale = Mathf.Clamp(Time.timeScale, 0, 1);
+            /*if (recoverTime == 1)
+            {
+                recover();
+            }*/
         }
     }
-
+    /*public void recover()
+    {
+        Time.timeScale = Mathf.Lerp(Time.timeScale, 1, 0.5f);
+    }*/
     public void slow()
     {
         Time.timeScale = slowDownFactor;
-        Time.fixedDeltaTime = Time.timeScale * .02f;
+        Time.fixedDeltaTime = Time.timeScale * slowDownFactor;
+        //recoverTime = slowDownFactor;
     }
 
     
