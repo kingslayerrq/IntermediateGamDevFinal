d731017f9d0982110ffc42cba41856bb00448859
Puddin26 <jl12476@nyu.edu>
2023-11-26 00:31:40 -0500

    Finished Floor Enemy movements

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed;
     public Transform groundDetection;
     private bool movingRight = true;
@@ -12,44 +11,38 @@ public class EnemyControl : MonoBehaviour
     // Layer mask to detect platforms
     private LayerMask platformLayerMask;
 
-    // Start is called before the first frame update
     void Start()
     {
         platformLayerMask = LayerMask.GetMask("Platform");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (movingRight)
-        {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-        }
-        else{
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-        }
-        
+        Move();
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f, platformLayerMask);
         Debug.DrawLine(groundDetection.position, groundDetection.position + new Vector3(0, -1f, 0), Color.red);
 
-        if (groundInfo.collider == true)
-        {
-            Debug.Log("On Platform");
-        }
         if (groundInfo.collider == false)
         {
-            Debug.Log("On Edge");
-            if (movingRight == true)
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = true;
-            }
+            Debug.Log("Edge");
+            Flip();
         }
+    }
+
+    void Move()
+    {
+        Vector2 moveDirection = (transform.localScale.x > 0) ? Vector2.right : Vector2.left;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        movingRight = !movingRight;
+
+        // Flip the sprite by scaling
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
