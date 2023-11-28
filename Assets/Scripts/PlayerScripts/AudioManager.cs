c7af5c7c014b5b537503d7c936f1ce74675840dc
Jamaricodeslol <jkmm0444@gmail.com>
2023-11-28 13:02:42 -0500

    FixedAudioManager
    A complete Audio Management :]

---
+++ Assets/Scripts/PlayerScripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource playerAudio;
+
+    public AudioClip Jump;
+    public AudioClip Dash;
+    //public AudioSource Land;
+    public AudioClip Slash;
+
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlayJumpSFX()
+    {
+        playerAudio.clip = Jump;
+        playerAudio.Play();
+    }
+
+    public void PlayDashSFX()
+    {
+        playerAudio.clip = Dash;
+        playerAudio.Play();
+    }
+
+    public void PlaySlashSFX()
+    {
+        playerAudio.clip = Slash;
+        playerAudio.Play();
+    }
+
+
+}
