65c59500c20f2e95e30a21c8d471d26d32a18887
kingslayerrq <ruizhequ813@gmail.com>
2023-11-28 22:47:19 -0500

    fixed dash bug and reformatted a little code

--- Assets/Scripts/PlayerScripts/PlayerAudioManager.cs
+++ Assets/Scripts/PlayerScripts/PlayerAudioManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+public class PlayerAudioManager : MonoBehaviour
 {
     public AudioSource playerAudio;
 
